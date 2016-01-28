a063d8c04818197250e3ca2a331b7a8b4d500ed0
Rui Gil <ruipgil@gmail.com>
2016-01-28 15:00:49 +0000

    Add transportation mode inferring

--- tracktotrip/__init__.py
+++ tracktotrip/__init__.py
@@ -2,4 +2,5 @@ from .segment import segment as segment
 from .smooth import *
 from .preprocess import preprocess as preprocess
 from .simplify import simplify
+from .transport import transport
 from . import utils
