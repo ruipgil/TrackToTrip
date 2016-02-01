f4ef91addadfdd5ef8bceb2904bc149e4e843a85
Rui Gil <ruipgil@gmail.com>
2016-02-01 19:42:48 +0000

    Add top level function to go from track to trip

--- tracktotrip/__init__.py
+++ tracktotrip/__init__.py
@@ -4,4 +4,5 @@ from .preprocess import preprocess as preprocess
 from .simplify import simplify
 from .transport import transport
 from .noiseDetection import noiseDetection
+from .tracktotrip import tracktotrip
 from . import utils
