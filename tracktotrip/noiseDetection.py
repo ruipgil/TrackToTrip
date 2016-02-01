9d89f1d587ac05d9a8407a5bd0aa201596be0f4a
Rui Gil <ruipgil@gmail.com>
2016-02-01 17:04:53 +0000

    Document and refactor noise detection

--- tracktotrip/noiseDetection.py
+++ tracktotrip/noiseDetection.py
@@ -1,18 +1,32 @@
 import matplotlib.pyplot as plt
-from sklearn.cluster import DBSCAN
-from sklearn.preprocessing import StandardScaler
 import numpy as np
-import math
 from sklearn.preprocessing import normalize
-from scipy import stats
 
 def normalizeTrack(track):
+    """
+    Normalizes track points and returns the result as an array of 2D arrays
+    """
     return normalize(map(lambda p: [p.getLat(), p.getLon()], track))
 
+def normalizeDots(dots):
+    """
+    Normalize numbers in an array
+    """
+    m = max(dots)
+    return map(lambda d: d/m, dots)
+
 def dot(points):
+    """
+    Calculates the dot product between every two vectors and returns an array with it
+    :param points: Array of 2D arrays
+    """
     return map(lambda i: np.dot(points[i], points[i+1]), range(len(points)-1))
 
 def distance(arr):
+    """
+    Returns the distance between every two consecutive points in an array
+    :param arr: Array of numbers
+    """
     last = arr[0]
     result = []
     for elm in arr:
@@ -20,49 +34,67 @@ def distance(arr):
         last = elm
     return result
 
-def strategy(sample, av, var):
-    threshold = av+var
-    result = []
-    for i, s in enumerate(sample):
-        if s > threshold:
-            result.append(i)
-    return result
+def plot(track, dots, noiseSet, average, variance):
+    """
+    Plot dot product of a track, alongside average and variance. Noise points
+        are shown as dots
 
-def noiseDetection(track):
-    normalized = normalizeTrack(track)
-    dots = dot(normalized)
-    dots = distance(dots)
-    m = max(dots)
-    dots = map(lambda d: d/m, dots)
-    dotsMapped = list(enumerate(dots))
-    dotsMapped = filter(lambda dot: dot[1]!=0, dotsMapped)
-    dotsForStats = map(lambda r: r[1], dotsMapped)
-
-    average = np.median(dotsForStats)
-    variance = np.var(dotsForStats)
-
-    WINDOW = 10
-    SLIDE = 1
-
-    noise = []
-    for i in range(len(dotsMapped)-WINDOW):
-        index = i
-        start = index
-        end = index+WINDOW
-        sample = dotsMapped[start:end]
-        noises = strategy4(map(lambda d: d[1], sample), average, variance)
-        for n in noises:
-            noise.append(sample[n][0])
-
-    noise = set(noise)
+    Returns the matplotlib plot
+    """
+    print(average, variance, average-variance, average+variance)
 
     plt.plot(dots)
-    print(average, variance, average-variance, average+variance)
     plt.plot([0, len(dots)], [average, average])
     plt.plot([0, len(dots)], [average-variance, average-variance])
     plt.plot([0, len(dots)], [average+variance, average+variance])
-    for n in noise:
+    for n in noiseSet:
         plt.plot(n, dots[n], 'o')
-    plt.show()
+
+    return plt
+
+def noiseDetection(track):
+    """
+    Method to detect noise in the signal.
+
+    The principal behind the detection is based on the changes in direction.
+    Usually when there is noise in a GPS signal there will be points "very"
+        distant and in different directions.
+    We detect noise in the signal when those changes in direction are too
+        abrupt, using the *dot product* between a point and the next one.
+    The process is as follows:
+        1. Normalize points
+        2. Calculate the difference between two consecutive points, where
+            the first is 0
+        3. Normalize the differences
+        4. Calculate the median and variance of the normalized differences
+            and removing zero (stationary values)
+        5. Values above *median + variance* are dropped
+
+    The argument is a track: an array of points (Point).
+    The return is an array with the indices of the points marked as noise
+        relative to the argument.
+
+    This approach doesn't take into consideration time intervals, just time
+        sequence.
+    """
+    normalized = normalizeTrack(track)
+
+    dots = dot(normalized)
+    dots = distance(dots)
+    dots = normalizeDots(dots)
+
+    dotsMapped = list(enumerate(dots))
+    dotsMapped = filter(lambda dot: dot[1]!=0, dotsMapped)
+
+    dotsForStats = map(lambda r: r[1], dotsMapped)
+    average = np.median(dotsForStats)
+    variance = np.var(dotsForStats)
+    threshold = average + variance
+
+    noise = []
+    for i, d in dotsMapped:
+        if d > threshold:
+            noise.append(i)
+
     return noise
 
