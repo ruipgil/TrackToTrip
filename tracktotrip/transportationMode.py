d75e30025e3eb69afb86a385af73e54724e2b9ba
Rui Gil <ruipgil@gmail.com>
2016-05-19 16:32:29 +0100

    Add transportation mode infering based on decision tree classification

--- tracktotrip/transportationMode.py
+++ tracktotrip/transportationMode.py
@@ -1,19 +1,156 @@
-def inferTransportationMode(points):
-    tmodes = naiveTransportationInferring(points)
-    tgroup = group(tmodes)
-    result = []
-    for j, (i, tm) in enumerate(tgroup):
-        if len(tgroup) <= j + 1:
-            end, _ = tgroup[j]
+from sklearn import tree
+import matplotlib.pyplot as plt
+from .changepoint import changepoint
+import numpy as np
+
+def inferTransportationMode(points, method="Changepoint", removeStops=False):
+    if method == "Naive":
+        tmodes = naiveTransportationInferring(points)
+        tgroup = group(tmodes)
+        result = []
+        for j, (i, tm) in enumerate(tgroup):
+            if len(tgroup) <= j + 1:
+                end, _ = tgroup[j]
+            else:
+                end, _ = tgroup[j + 1]
+            # if tm != 'Stop':
+            result.append({
+                'label': tm,
+                'from': i,
+                'to': end
+                })
+        return result
+    elif method == "Changepoint":
+        tmodes = speedClusteringTransportationInfering(points)
+        print(tmodes)
+        if removeStops:
+            return filter(lambda tm: tm['label'] != 'Stop', tmodes)
         else:
-            end, _ = tgroup[j + 1]
-        # if tm != 'Stop':
-        result.append({
-            'label': tm,
-            'from': i,
-            'to': end
+            return tmodes
+
+def generateDatasetOn(arr, T=1, F=0, interpolate=1):
+    N = 100
+    X = []
+    Y = []
+
+    def fill(val, pb):
+        # print(val, pb)
+        samples = int(pb * N)
+        counterSamples = N - samples
+        for i in range(samples):
+            X.append([val])
+            Y.append(T)
+        for i in range(counterSamples):
+            X.append([val])
+            Y.append(F)
+
+    pastCls = None
+    for cls in arr:
+        val = cls['val']
+        pb = cls['prob']
+        fill(val, pb)
+        if pastCls != None:
+            IN = interpolate + 1
+            iVal = (pastCls['val']-val)/IN
+            iPb = (pastCls['prob']-pb)/IN
+            for j in range(1, IN):
+                fill((j * iVal) + val, (j * iPb) + pb)
+        pastCls = cls
+    return X, Y
+
+def buildSoftClassifier(steps):
+    X, Y = generateDatasetOn(steps, interpolate=100)
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(X, Y)
+    def getProb(val):
+        return clf.predict_proba([[val]])[0][1]
+    return getProb, clf
+
+def plotSoftClassifier(clf, step, N=100):
+    stp = step/N
+    res = []
+    for i in range(N):
+        v = stp * i
+        res.append([
+            v, clf.predict_proba([[v]])
+            ])
+    plt.plot(map(lambda a: a[0], res), map(lambda a: a[1][0][1], res))
+
+stop, stopdt = buildSoftClassifier([{ 'val': 0, 'prob': 1 }, { 'val': 1, 'prob': 0.9 }, { 'val': 5, 'prob': 0.18 }, { 'val': 6.5, 'prob': 0 }])
+walk, walkdt = buildSoftClassifier([{ 'val': 0, 'prob': 0 }, { 'val': 2, 'prob': 0.1 }, { 'val': 5, 'prob': 0.8 }, { 'val': 6.5, 'prob': 0.95 }, { 'val': 8, 'prob': 0.8 }, { 'val': 9, 'prob': 0.1 }, { 'val': 10, 'prob': 0 }])
+run, rundt = buildSoftClassifier([{ 'val': 6.5, 'prob': 0 }, { 'val': 7, 'prob': 0.1 }, { 'val': 11, 'prob': 0.86 }, { 'val': 22, 'prob': 0.2 }, { 'val': 28, 'prob': 0 }])
+bike, bikedt = buildSoftClassifier([{ 'val': 6.5, 'prob': 0 }, { 'val': 7, 'prob': 0.2 }, { 'val': 10, 'prob': 0.2 }, { 'val': 20, 'prob': 0.7 }, { 'val': 21, 'prob': 0.7 }, { 'val': 22, 'prob': 0.5 }, { 'val': 38, 'prob': 0 }])
+vehicle, vehicledt = buildSoftClassifier([{ 'val': 9, 'prob': 0 }, { 'val': 10, 'prob': 0.05 }, { 'val': 20, 'prob': 0.05 }, { 'val': 38, 'prob': 0.9 }, { 'val': 40, 'prob': 1 }])
+
+def softClassifySpeed(speed):
+    return [
+            stop(speed),
+            walk(speed),
+            # run(speed),
+            # bike(speed),
+            vehicle(speed)
+            ]
+
+
+CHANGE_TIME_THRESHOLD = 10 #60 seconds
+def speedClusteringTransportationInfering(points):
+    vels = map(lambda p: p.vel, points)
+    # get changepoint indexes
+    cp = changepoint(vels)
+    # insert last point to be a change point
+    cp.append(len(points) - 1)
+
+    # info for each changepoint
+    cp_info = []
+
+    for i in range(0, len(cp) - 1):
+        fromIndex = cp[i]
+        toIndex = cp[i+1]
+
+        sp = np.mean(vels[fromIndex:toIndex])
+        probs = softClassifySpeed(sp)
+        cp_info.append({
+            'from': fromIndex,
+            'to': toIndex,
+            'dt': points[toIndex].getTimestamp() - points[fromIndex].getTimestamp(),
+            'average_speed': sp,
+            # 'classification': probs,
+            'label': choose_class(probs)
             })
-    return result
+
+    # group based on label
+    previous = cp_info[0]
+    grouped = []
+    cum_dt = cp_info[0]['dt']
+
+    print(len(cp_info))
+    print(len(points))
+    print(cp_info)
+
+    for cp in cp_info[1:]:
+        if cp['label'] != previous['label'] and cp['dt'] > CHANGE_TIME_THRESHOLD:
+            previous['to'] = cp['from']
+            previous['dt'] = cum_dt
+            grouped.append(previous)
+            previous = cp
+            cum_dt = 0
+        cum_dt = cum_dt + cp['dt']
+    previous['to'] = cp_info[-1]['to']
+    grouped.append(previous)
+
+
+    for g in grouped:
+        print(g['label'], g['dt'], g['from'], g['to'])
+
+    return grouped
+
+def choose_class(elm):
+    reprs = ['Stop',
+            'Walk',
+            # 'Run',
+            # 'Bike',
+            'Vehicle']
+    return reprs[np.argmax(elm)]
 
 def naiveTransportationInferring(points, S = 0.1, W = 7):
     """Infers the transportation mode of a set of points
