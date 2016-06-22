f7803e8a1759a453e1fbe6b1ac8139cbf53d9799
Rui Gil <ruipgil@gmail.com>
2016-06-22 01:11:22 +0100

    Add pypi information and requirements

--- setup.py
+++ setup.py
@@ -1,19 +1,13 @@
-from setuptools import setup
-
-setup(name='TrackToTrip',
-      version='0.0.1',
-      description='GPS processing library',
-      url='http://github.com/ruipgil/TrackToTrip',
-      author='Rui Gil',
-      author_email='ruipgil@gmail.com',
-      license='MIT',
-      packages=['tracktotrip'],
-      install_requires=[
-          'gpxpy',
-          'numpy',
-          'pykalman',
-          'sklearn'
-      ],
-      test_suite='nose.collector',
-      tests_require=['nose'],
-      zip_safe=False)
+from distutils.core import setup
+setup(
+  name = 'tracktotrip',
+  packages = ['tracktotrip'],
+  version = '0.1',
+  description = 'Track processing library',
+  author = 'Rui Gil',
+  author_email = 'ruipgil@gmail.com',
+  url = 'https://github.com/ruipgil/TrackToTrip',
+  download_url = 'https://github.com/ruipgil/TrackToTrip/archive/master.zip',
+  keywords = ['track', 'trip', 'GPS', 'GPX'],
+  classifiers = [],
+)
