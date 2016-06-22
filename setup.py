9885f28a5a0e85f053ac68194f27d8f23edde713
Rui Gil <ruipgil@gmail.com>
2016-06-22 01:55:07 +0100

    Fix dependencies not installing

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from distutils.core import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
   name = 'tracktotrip',
   packages = ['tracktotrip'],
-  version = '0.1.1',
+  version = '0.1.2',
   description = 'Track processing library',
   author = 'Rui Gil',
   author_email = 'ruipgil@gmail.com',
@@ -10,4 +14,5 @@ setup(
   download_url = 'https://github.com/ruipgil/TrackToTrip/archive/master.zip',
   keywords = ['track', 'trip', 'GPS', 'GPX'],
   classifiers = [],
+  install_requires=required,
 )
