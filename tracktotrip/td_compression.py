c4954757a766c83fb4aac3960cfa4f0d2e7ac0eb
Rui Gil <ruipgil@gmail.com>
2016-07-28 01:52:05 +0100

    Add spt compression function

--- tracktotrip/td_compression.py
+++ tracktotrip/td_compression.py
@@ -103,3 +103,60 @@ def td_tr(points, dist_threshold):
             return one
         else:
             return [points[0], points[-1]]
+
+def spt(points, max_dist_error, max_speed_error):
+    """ A combination of both `td_sp` and `td_tr`
+
+    Detailed in,
+        Spatiotemporal Compression Techniques for Moving Point Objects,
+        Nirvana Meratnia and Rolf A. de By, 2004,
+        in Advances in Database Technology - EDBT 2004: 9th
+        International Conference on Extending Database Technology,
+        Heraklion, Crete, Greece, March 14-18, 2004
+
+    Args:
+        points (:obj:`list` of :obj:`Point`)
+        max_dist_error (float): max distance error, in meters
+        max_speed_error (float): max speed error, in km/h
+    Returns:
+        :obj:`list` of :obj:`Point`
+    """
+    if len(points) <= 2:
+        return points
+    else:
+        is_error = False
+        e = 1
+        while e < len(points) and not is_error:
+            i = 1
+            while i < e and not is_error:
+                delta_e = time_dist(points[e], points[0]) / 3600
+                delta_i = time_dist(points[i], points[0]) / 3600
+
+                di_de = delta_i / delta_e
+                d_lat = points[e].lat - points[0].lat
+                d_lon = points[e].lon - points[0].lon
+                point = Point(
+                    points[0].lat + d_lat * di_de,
+                    points[0].lon + d_lon * di_de,
+                    None
+                )
+
+                dt1 = time_dist(points[i], points[i-1])
+                if dt1 == 0:
+                    dt1 = 0.000000001
+                dt2 = time_dist(points[i+1], points[i])
+                if dt2 == 0:
+                    dt2 = 0.000000001
+
+                v_i_1 = loc_dist(points[i], points[i-1]) / dt1
+                v_i = loc_dist(points[i+1], points[i]) / dt2
+
+                if loc_dist(points[i], point) > max_dist_error or abs(v_i - v_i_1) > max_speed_error:
+                    is_error = True
+                else:
+                    i = i + 1
+            if is_error:
+                return [points[0]] + spt(points[i:len(points)], max_dist_error, max_speed_error)
+            e = e + 1
+        if not is_error:
+            return [points[0], points[len(points)-1]]
