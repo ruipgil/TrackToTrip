2f281edda17ca6f867baa87dd11024849b71d950
Rui Gil <ruipgil@gmail.com>
2016-07-28 16:48:23 +0100

    Update dependencies

--- setup.py
+++ setup.py
@@ -1,29 +1,16 @@
 """
 Setup script
 """
+import os
 from distutils.core import setup
 
-REQS = [
-    'gpxpy',
-    'requests',
-    'numpy==1.11.0',
-    'pandas==0.18.1',
-    'pykalman==0.9.5',
-    'python-dateutil==2.5.3',
-    'pytz==2016.4',
-    'Rtree==0.8.2',
-    'scikit-learn==0.17.1',
-    'scipy==0.17.1',
-    'six==1.10.0',
-    'sklearn==0.0',
-    'ikalman',
-    'changepy'
-]
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
 
 setup(
     name='tracktotrip',
     packages=['tracktotrip'],
-    version='0.2.2',
+    version='0.3',
     description='Track processing library',
     author='Rui Gil',
     author_email='ruipgil@gmail.com',
@@ -31,5 +18,5 @@ setup(
     download_url='https://github.com/ruipgil/TrackToTrip/archive/master.zip',
     keywords=['track', 'trip', 'GPS', 'GPX'],
     classifiers=[],
-    install_requires=REQS
+    install_requires=read('requirements.txt').split('\n')
 )
